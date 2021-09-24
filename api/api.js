325882890a72a7069e16dab4afe7d61cb61c4db9
La7rodectus <maxmega27@gmail.com>
2021-09-24 14:34:00 +0300

    + API

---
+++ api/api.js
@@ -0,0 +1,22 @@
+const https = require('http');
+const path = require('path');
+let fs = require('fs');
+const getRoute = require('./apiRouter.js').getRoute;
+
+const PORT = process.env.port || 7070;
+const URL = 'https://localhost:';
+
+const execReq = async (req, res) => {
+  console.log('New API REQ:', req.url);
+  //const accessLevel = await getUserAccess(req);
+  const route = getRoute(req.url, /*accessLevel*/);
+  const content = await route.executor(req);
+  res.setHeader('Content-Type', route.contentType);
+  res.end(content);
+};
+
+http.createServer(async (req, res) => {
+  execReq(req, res);
+}).listen(PORT);
+
+console.log(`Api listen on ${PORT}`);
--- src/App.vue
+++ src/App.vue
@@ -10,16 +10,14 @@ import someMixin from './mixins/someMixin.js';
 export default {
   name: 'App',
   mixins: [someMixin], //global mixins
+  asyncData({ storage }) {
+    
+  }
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
